efd4475fb3a381aa3d600dfdfac3cfc668ed89d7
Alex Golovanov <alex.golovanov@gmail.com>
2023-07-26 16:29:44 +0300

    feat: add triangle component

--- app/components/Triangle/Triangle.tsx
+++ app/components/Triangle/Triangle.tsx
@@ -1,3 +1,30 @@
-export const Triangle = () => {
-  return <div>triangle</div>;
+import styles from './styles.module.css';
+import clsx from 'clsx';
+interface IProps {
+  data: number[][];
+  path: number[];
+  values: number[];
+  total: number;
+}
+
+export const Triangle = (props: IProps): JSX.Element => {
+  const { data, path } = props;
+  return (
+    <div className={styles.root}>
+      {data.map((row, rowKey) => (
+        <div key={rowKey} className={styles.row}>
+          {row.map((number, numberKey) => (
+            <div
+              key={numberKey}
+              className={clsx(styles.number, {
+                [styles.numberHighlight]: numberKey === path[rowKey],
+              })}
+            >
+              {number}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
