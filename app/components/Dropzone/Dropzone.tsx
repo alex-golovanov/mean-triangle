f6eff7cc45592fc5c012aeecd1528c72fad6d58f
Alexander Golovanov <alex.golovanov@gmail.com>
2023-07-27 18:19:07 +0300

    perf: styled dropzone

--- app/components/Dropzone/Dropzone.tsx
+++ app/components/Dropzone/Dropzone.tsx
@@ -2,14 +2,18 @@
 
 import { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import clsx from 'clsx';
+
+import styles from './styles.module.css';
 
 interface IProps {
   onLoad?: (str?: string) => void;
   onError?: (code: string) => void;
+  className?: string;
 }
 
 export const Dropzone = (props: IProps): JSX.Element => {
-  const { onLoad, onError } = props;
+  const { onLoad, onError, className } = props;
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
       const reader = new FileReader();
@@ -27,12 +31,12 @@ export const Dropzone = (props: IProps): JSX.Element => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className={clsx(className, styles.root)}>
       <input {...getInputProps()} />
       {isDragActive ? (
-        <p>Drop the files here ...</p>
+        <p>Drop files here ...</p>
       ) : (
-        <p>Drag & drop some files here, or click to select files</p>
+        <p>Drag & drop files here or click to select</p>
       )}
     </div>
   );
